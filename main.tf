0902eb391a75a53bb5e6f1e357bf90fc3aa8766a
shafiunp <phatanshafiun@gmail.com>
2023-02-23 22:10:54 +0400

    updated main.tf file

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-0e742cca61fb65051" # us-west-2
   instance_type = "t2.micro"
   key_name= "aws_key"
-    #vpc_security_group_ids = [aws_security_group.my_security_group.id]
+    vpc_security_group_id = [aws_security_group.my_security_group.id]
     
 
   network_interface {
