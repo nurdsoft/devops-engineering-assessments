88b9d0683eee2ac5d8e6ebbab2b826b51d4eb6ed
shafiunp <phatanshafiun@gmail.com>
2023-02-24 00:18:21 +0400

    updated outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -4,9 +4,3 @@ output "my_instance_instance_id" {
 output "my_instance_public_ip_address" {
   value = aws_instance.my_instance.public_ip
 }
-output "my_vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
-output "my_subnet_id" {
-  value = aws_subnet.my_subnet.id
-}
