a7d1cd84a332f8f709946b70541e81286371fb7c
shafiunp <phatanshafiun@gmail.com>
2023-02-23 22:22:39 +0400

    updated main.tf file

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-0e742cca61fb65051" # us-west-2
   instance_type = "t2.micro"
   key_name= "aws_key"
-    vpc_security_group_id = [aws_security_group.my_security_group.id]
+    #vpc_security_group_ids = [aws_security_group.my_security_group.id]
     
 
   network_interface {
@@ -50,6 +50,10 @@ resource "aws_instance" "my_instance" {
   }
 
   
+}
+resource "aws_network_interface_sg_attachment" "my_sg_attachment" {
+  security_group_id    = aws_security_group.my_security_group.id
+  network_interface_id = aws_instance.my_instance.primary_network_interface_id
 }
  resource "aws_security_group" "my_security_group" {
   egress = [
