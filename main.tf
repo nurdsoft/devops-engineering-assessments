32f606084dc7aa9278fa79fa7861a7a4c3f44188
shafiunp <phatanshafiun@gmail.com>
2023-02-23 21:03:48 +0400

    updated main.tf file

--- main.tf
+++ main.tf
@@ -40,6 +40,9 @@ resource "aws_network_interface" "my_network_interface" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-0e742cca61fb65051" # us-west-2
   instance_type = "t2.micro"
+  key_name= "aws_key"
+    vpc_security_group_ids = [aws_security_group.my_security_group.id]
+    
 
   network_interface {
     network_interface_id = aws_network_interface.my_network_interface.id
@@ -50,4 +53,33 @@ resource "aws_instance" "my_instance" {
     cpu_credits = "unlimited"
   }
 }
+resource "aws_security_group" "my_security_group" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+}
+
 
