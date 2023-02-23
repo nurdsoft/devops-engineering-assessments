22877b822b0d739546bb429336a1323c137f0ca9
shafiunp <phatanshafiun@gmail.com>
2023-02-23 22:33:56 +0400

    updated main.tf file

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "my_instance" {
 }
 resource "aws_network_interface_sg_attachment" "my_sg_attachment" {
   security_group_id    = aws_security_group.my_security_group.id
-  network_interface_id = aws_instance.my_instance.primary_network_interface_id
+  network_interface_id = aws_network_interface.my_network_interface.id
 }
  resource "aws_security_group" "my_security_group" {
   egress = [
