5b32170a3144780c4093cebda81687c50eb19954
shafiunp <phatanshafiun@gmail.com>
2023-02-24 11:32:05 +0400

    updated main.tf file variables.file

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" { 
   # Configuration options
-  region = var.my_instance_region
+  region = var.region
 }
 
 # Create VPC
@@ -80,8 +80,8 @@ Name = "SSH Security Group"
 #Create a new EC2 launch configuration
 resource "aws_instance" "my_instance" {
 ami                    = "ami-0e742cca61fb65051"
-instance_type               = var.my_instance_instance_type
-key_name                    = var.my_instance_key_name
+instance_type               = var.instance_type
+key_name                    = var.key_name
 security_groups             = [aws_security_group.my_security_group.id]
 subnet_id                   = aws_subnet.my_public_subnet.id
 associate_public_ip_address = true
--- variables.tf
+++ variables.tf
@@ -4,12 +4,12 @@ description = "enter vpc value"
 variable "my_instance_subnet_id" {
 description = "enter subnet value" 
 } */
-variable "my_instance_region" {
+variable "region" {
 description = "enter region value"
 }
-variable "my_instance_key_name" {
+variable "key_name" {
 description = "enter key value"  
 }
-variable "my_instance_instance_type" {
+variable "instance_type" {
 description = "enter instance type value" 
 }
