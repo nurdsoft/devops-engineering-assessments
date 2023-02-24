58410c70e758a2839f6f03ab7ed5224d4fee271b
shafiunp <phatanshafiun@gmail.com>
2023-02-24 21:16:06 +0400

    updated main.tf file

--- main.tf
+++ main.tf
@@ -12,55 +12,13 @@ provider "aws" {
   region = var.region
 }
 
-# Create VPC
-resource "aws_vpc" "my_vpc" {
-  id = var.vpc_id
-cidr_block = "10.0.0.0/16"
-enable_dns_hostnames    = true
-tags      = {
-Name    = "my_VPC"
-}
-}
-# Create Internet Gateway
-resource "aws_internet_gateway" "my_internet_gateway" {
-vpc_id    = aws_vpc.my_vpc.id
-tags = {
-Name  = "internet_gateway"
-}
-}
-# Create Public Subnet
-resource "aws_subnet" "my_public_subnet" {
-  id = var.subnet_id
-vpc_id                  = aws_vpc.my_vpc.id
-cidr_block              = "10.0.0.0/24"
-availability_zone       = "ap-south-1b"
-map_public_ip_on_launch = true
-tags      = {
-Name    = "public-subnet"
-}
-}
-# Create Route Table
-resource "aws_route_table" "my_public_route_table" {
-vpc_id       = aws_vpc.my_vpc.id
-route {
-cidr_block = "0.0.0.0/0"
-gateway_id = aws_internet_gateway.my_internet_gateway.id
-}
-tags       = {
-Name     = "Public Route Table"
-}
-}
-# Associate Public Subnet
-resource "aws_route_table_association" "my_public_subnet_route_table_association" {
-subnet_id           = aws_subnet.my_public_subnet.id
-route_table_id      = aws_route_table.my_public_route_table.id
-}
+
 
 # Create Security Group 
 resource "aws_security_group" "my_security_group" {
 name        = "SSH Security Group"
 description = "Enable SSH access on Port 22"
-vpc_id      = aws_vpc.my_vpc.id
+vpc_id      = var.vpc_id
 ingress {
 description      = "SSH Access"
 from_port        = 22
@@ -85,7 +43,7 @@ ami                    = "ami-0e742cca61fb65051"
 instance_type               = var.instance_type
 key_name                    = var.key_name
 security_groups             = [aws_security_group.my_security_group.id]
-subnet_id                   = aws_subnet.my_public_subnet.id
+subnet_id                   = var.subnet_id
 associate_public_ip_address = true
 lifecycle {
 create_before_destroy = true
