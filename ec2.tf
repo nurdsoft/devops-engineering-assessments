d61ddf8bd37633ec9a3c5aad1a612f76fe200998
Venkatesh athiyannan <31950175+wisevenkat@users.noreply.github.com>
2023-04-23 03:53:40 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -22,10 +22,22 @@ resource "aws_security_group" "Allow" {
     to_port     = 22
     protocol    = "tcp"
   }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "ec2-web" {
-  ami                         = "ami-02396cdd13e9a1257"
+  ami                         = "ami-016eb5d644c333ccb"
   instance_type               = var.instance_type
   key_name                    = var.key_name
   vpc_security_group_ids      = [aws_security_group.Allow.id]
