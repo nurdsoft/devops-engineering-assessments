4d48d7a097e273663a9e3d35e9daf245e65e8e73
Arnav <arnavchachra.ac@gmail.com>
2023-07-20 19:32:44 +0530

    Example EC2 Instance

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  tags = {
+    Name = "Example EC2 Instance"
+  }
+}
+
+output "InstanceId" {
+  value = aws_instance.example.id
+}
+
+output "PublicIpAddress" {
+  value = aws_instance.example.public_ip
+}
