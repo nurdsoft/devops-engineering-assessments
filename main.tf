3a7465d3e70a2b11bab08e860ead573a6e3e8871
Arnav <arnavchachra.ac@gmail.com>
2023-07-20 22:00:18 +0530

    Test Commit

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example02" {
   ami           = "ami-0c94855ba95c71c99"
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
   key_name      = var.key_name
 
   tags = {
-    Name = "Example Test EC2 Instance"
+    Name = "Example EC2 Instance"
   }
 }
 
