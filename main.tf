4f270c11b7f6bc294a15d209e233abea33c10ff6
Arnav <arnavchachra.ac@gmail.com>
2023-07-24 18:35:09 +0530

    Test for forked PR

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example02" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  tags = {
+    Name = "Example EC2 Instance"
+  }
+}
+
+output "InstanceId" {
+  value = aws_instance.example02.id
+}
+
+output "PublicIpAddress" {
+  value = aws_instance.example02.public_ip
+}
