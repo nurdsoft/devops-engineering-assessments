18cb83cc6beb40283670434ae4a281262c6c1717
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-12-10 04:00:41 -0500

    add: enhance EC2 module outputs with descriptions for clarity

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,7 +1,9 @@
 output "instance_id" {
   value = aws_instance.ec2.id
+  description = "The ID of the EC2 instance"
 }
 
 output "public_ip" {
   value = aws_instance.ec2.public_ip
+  description = "The public IP address of the EC2 instance"
 }
