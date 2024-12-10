b92ce7f8d78859311d9045bed4c3e581960e8602
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-12-10 04:00:54 -0500

    add: define types for EC2 module variables for better validation

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,24 +1,30 @@
 variable "region" {
   description = "The region in which the resources will be deployed"
+  type = string
 }
 
 variable "key_name" {
   description = "The name of the key pair to use for the EC2 instances"
+  type = string
 }
 
 variable "instance_type" {
   description = "The type of EC2 instance to launch"
+  type = string
 }
 
 variable "subnet_id" {
   description = "The ID of the subnet in which the EC2 instances will be deployed"
+  type = string
 }
 
 variable "vpc_id" {
   description = "The ID of the VPC in which the EC2 instances will be deployed"
+  type = string
 }
 
 variable "ami" {
   description = "The AMI to use for the EC2 instances"
   default = "ami-006dcf34c09e50022"
+  type = string
 }
