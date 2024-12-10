fbe925a0c61428ba0720b60f283e0cc71fc27556
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-12-10 04:03:26 -0500

    format: align variable type definitions for consistency

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "region" {
   description = "The region in which the resources will be deployed"
-  type = string
+  type        = string
 }
 
 variable "key_name" {
   description = "The name of the key pair to use for the EC2 instances"
-  type = string
+  type        = string
 }
 
 variable "instance_type" {
   description = "The type of EC2 instance to launch"
-  type = string
+  type        = string
 }
 
 variable "subnet_id" {
   description = "The ID of the subnet in which the EC2 instances will be deployed"
-  type = string
+  type        = string
 }
 
 variable "vpc_id" {
   description = "The ID of the VPC in which the EC2 instances will be deployed"
-  type = string
+  type        = string
 }
