bc530db1a4105a04d4e71ce7332fe0d18a6f87b5
Juan Carlos Alvarez <alvarezvelez.juancarlos@gmail.com>
2024-12-14 02:10:40 -0600

    using AMI lookup

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
 terraform {
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "my_instance" {
-  ami                    = "ami-0e2c8caa4b6378d8c" # Example AMI ID, change as needed
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   key_name               = var.key_name
