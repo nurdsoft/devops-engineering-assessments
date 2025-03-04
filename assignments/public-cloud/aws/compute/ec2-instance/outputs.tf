a633546685b06a7373e8fce6b12be3168b6b8217
wallano <wallano@gmail.com>
2025-03-04 16:07:10 -0500

    Add files via upload
    This commit contains five files:
    - main.tf
    - variables.tf
    - outputs.tf
    - screenshot-assignment.png
    - screenshotwithoutputs.png
    
    The last two files show images with the values of the created instance

---
+++ assignments/public-cloud/aws/compute/ec2-instance/outputs.tf
@@ -0,0 +1,10 @@
+#this file contains two outputs: InstanceId and PublicIpAddress
+output "InstanceId" {
+  description = "The ID of the instance"
+  value = aws_instance.example.id
+}
+
+output "PublicIpAddress" {
+  description = "The public IP address assigned to the instance, if applicable."
+  value = aws_instance.example.public_ip
+}
